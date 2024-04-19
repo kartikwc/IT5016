f33acb93a49efc6902c316eebaada930019818e6
kartikwc <167385718+kartikwc@users.noreply.github.com>
2024-04-19 12:33:13 +1200

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,11 @@
+
+M1 = float("enter m1:")
+M2 = float("enter m2:")
+M3 = float("enter m3:")
+M4 = float("enter m4:")
+grade = (M1+M2+M3+M4)/4
+if grade < 50:
+    print("pass")
+else:
+    print("fail")
+
